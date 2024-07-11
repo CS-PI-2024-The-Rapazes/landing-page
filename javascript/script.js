be1010e5beae3e012ff8916ac757b261a4d5d449
Rubens Matheus <y2afepolfxx@gmail.com>
2024-07-10 22:23:56 -0300

    se aplicado, este commit adicionará um slider para ativar o dark mode ou o light mode

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,18 @@ function modifierFont() {
     body.style.fontSize = 'x-large';
     
  }
+
+function changeIconTheme(modifierTheme) {
+    const id = modifierTheme.target.id; 
+    document.getElementById(id).src = "img/icons/toggle_on_b.svg";
+}
+
+function modifierTheme() {
+
+    const theme = document.body.getAttribute("data-tema");
+    const newTheme = theme == 'dark-mode' ? 'light-mode' : 'dark-mode';
+    document.body.setAttribute("data-tema", newTheme);
+    console.log(newTheme);
+}
+
+
