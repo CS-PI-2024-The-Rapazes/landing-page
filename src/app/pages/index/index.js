b7fceb28770ddbc99706c769069bb735296cc413
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-07-17 22:40:12 -0300

    feat: Alterar o botão

--- src/app/pages/index/index.js
+++ src/app/pages/index/index.js
@@ -1,3 +1,4 @@
+// MENU MOBILE DROPDOWN
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const nav = document.getElementById('nav');
@@ -5,4 +6,22 @@ document.addEventListener('DOMContentLoaded', function () {
     menuToggle.addEventListener('click', function () {
         nav.classList.toggle('active');
     });
+
+    const dropdownButton = document.getElementById('dropdownBotao');
+    const dropdownContent = document.getElementById('dropdownConteudo');
+
+    dropdownButton.addEventListener('click', function () {
+        dropdownContent.classList.toggle('show');
+    });
+
+
+    window.addEventListener('click', function (event) {
+        if (!event.target.matches('.dropdown-botao')) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    });
+
 });
+
