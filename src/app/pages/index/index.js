77ecf19e0c495a518aeb92387e79507cf947de17
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-07-18 02:38:45 -0300

    Alterar o menu de navegação e o botão de ajuda.

--- src/app/pages/index/index.js
+++ src/app/pages/index/index.js
@@ -1,27 +1,37 @@
 // MENU MOBILE DROPDOWN
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.getElementById('nav');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav");
 
-    menuToggle.addEventListener('click', function () {
-        nav.classList.toggle('active');
-    });
-
-    const dropdownButton = document.getElementById('dropdownBotao');
-    const dropdownContent = document.getElementById('dropdownConteudo');
-
-    dropdownButton.addEventListener('click', function () {
-        dropdownContent.classList.toggle('show');
-    });
-
-
-    window.addEventListener('click', function (event) {
-        if (!event.target.matches('.dropdown-botao')) {
-            if (dropdownContent.classList.contains('show')) {
-                dropdownContent.classList.remove('show');
-            }
-        }
-    });
-
+  menuToggle.addEventListener("click", function () {
+    nav.classList.toggle("active");
+  });
 });
 
+// BOTÃO TOGGLE
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownButton = document.getElementById("dropdownBotao");
+  const dropdownContent = document.getElementById("dropdownConteudo");
+
+  dropdownButton.addEventListener("click", function () {
+    dropdownContent.classList.toggle("show");
+  });
+
+  window.addEventListener("click", function (event) {
+    if (!event.target.matches(".dropdown-botao")) {
+      if (dropdownContent.classList.contains("show")) {
+        dropdownContent.classList.remove("show");
+      }
+    }
+  });
+});
+
+// BOX SHADOW NO MENU
+window.addEventListener("scroll", function () {
+  var topBar = document.querySelector(".top-bar");
+  if (window.scrollY > 50) {
+    topBar.classList.add("shadow");
+  } else {
+    topBar.classList.remove("shadow");
+  }
+});
