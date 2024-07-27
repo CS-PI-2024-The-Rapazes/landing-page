085e4d55d8b0a943cda270395fa1aeacbc862078
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-07-27 12:14:22 -0300

    Feat: Adicionar um icone check à lista de funcionalidades dos planos.

--- src/app/pages/index/index.js
+++ src/app/pages/index/index.js
@@ -15,35 +15,32 @@ window.addEventListener("scroll", function () {
   } else {
     nav.classList.remove("sombra");
   }
-
 });
 
 function modificaCard() {
-  document.getElementById('btn-plano').addEventListener('click', function () {
-    var cardNormal = document.getElementById('plano-normal');
-    var cardPremium = document.getElementById('plano-premium');
-    var btnText = document.getElementById('btn-plano');
+  document.getElementById("btn-plano").addEventListener("click", function () {
+    var cardNormal = document.getElementById("plano-normal");
+    var cardPremium = document.getElementById("plano-premium");
+    var btnText = document.getElementById("btn-plano");
 
-    if (cardNormal.style.display === 'none') {
-      cardNormal.style.display = 'flex';
-      cardPremium.style.display = 'none';
-      btnText.textContent = 'Plano: Premium';
+    if (cardNormal.style.display === "none") {
+      cardNormal.style.display = "flex";
+      cardPremium.style.display = "none";
+      btnText.textContent = "Plano: Premium";
     } else {
-      cardNormal.style.display = 'none';
-      cardPremium.style.display = 'flex';
-      btnText.textContent = 'Plano: Padrão';
+      cardNormal.style.display = "none";
+      cardPremium.style.display = "flex";
+      btnText.textContent = "Plano: Padrão";
     }
-    window.addEventListener('resize', function () {
+    window.addEventListener("resize", function () {
       var largura = window.innerWidth;
 
       if (largura >= 770) {
-        cardNormal.style.display = 'flex';
-        cardPremium.style.display = 'flex';
+        cardNormal.style.display = "flex";
+        cardPremium.style.display = "flex";
       }
-
     });
   });
 }
 
 modificaCard();
-
